9465491c11962aa38c3057020ccd31c7bafdbb0a
abhishekpatel <apatel23@calstatela.edu>
2021-01-07 08:40:49 -0800

    Fixed the Guess the Number game to accept negative numbers by using the lstrip function.

--- GuessingNumberGame.py
+++ GuessingNumberGame.py
@@ -19,8 +19,7 @@ def message():
  	
 def user_guess():
 	userGuess = input('Input your guess')
-	valid = userGuess.isdigit()
-	if userGuess.isdigit():
+	if userGuess.lstrip('-').isdigit():
 		if int(userGuess) > 1000 or int(userGuess) < 0:
 			print("Please enter a number between 0 & 1000")
 			return user_guess()
@@ -38,7 +37,7 @@ def game(rand1):
 	while guessRight and numOfGuess < 4:
 		if int(userInput) > rand1:
 			print("Guess: ",str(numOfGuess))
-			print("You Guessed ", userInput)
+			print("You Guessed: ", userInput)
 			print("Your guess is greater than the number")
 			guessRight = True
 			numOfGuess +=1
@@ -49,7 +48,7 @@ def game(rand1):
 				userInput= user_guess()
 		elif int(userInput) < rand1:
 			print("Guess: ", str(numOfGuess))
-			print("You Guessed ", userInput)
+			print("You Guessed: ", userInput)
 			print("Your guess is less than the number")
 			guessRight = True
 			numOfGuess +=1
@@ -62,25 +61,14 @@ def game(rand1):
 			numOfGuess +=1
 			print("Guess: ",str(numOfGuess))
 			print("You Guessed the correct number")
-			print("The Number was: ", str(rand1))
+			print("The Number is: ", str(rand1))
 			guessRight = False
 
-'''
-def test():
-	valid = input("enter a number")
-	valid2 = valid.isdigit()
-	if not valid2:
-		print("Please enter a number not a letter")
-		return test()
 
-	return valid
-'''
-		
 def main():
 	intro()
 	rand1 = random_number()
 	game(rand1)
-#	test()
 
 		
 main()
